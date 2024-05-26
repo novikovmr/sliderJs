95582ba0a7079ae543d96b32f1c35789b80d6566
novikovmr <maksim101101@gmail.com>
2024-05-26 16:00:16 +0400

    Реализовал бесконечную прокрутку через клонирование + удаление клонов

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const container = document.querySelector('.container');
 const slider = document.querySelector(".slider");
 const sliderLine = document.querySelector(".slider__line");
-const slides = document.querySelectorAll(".slider__slide");
+let slides = document.querySelectorAll(".slider__slide");
 const slide = document.querySelector(".slider__slide");
 
 const nextBtn = document.querySelector(".nextBtn");
 const prevBtn = document.querySelector(".prevBtn");
 
+let slidesList = sliderLine.children; //  HTML Collection со слайдами.
 let visibleSlides; // Кол-во слайдов, отображаемых на странице слайдера (В зависимости от ширины экрана)
 let slideWidth; // Ширина одного слайда (задаем в css)
 let count = 0; // Счетчик страниц слайдера.
@@ -17,7 +18,6 @@ function init() {
 
     slideWidth = slide.offsetWidth; // Получаем ширину слайда
     screenWidth = container.offsetWidth; // Получаем ширину контейнера
-    // console.log(screenWidth);
     if(screenWidth < 768) {
         visibleSlides = 1;
     }
@@ -30,27 +30,38 @@ function init() {
 
     // Задаем ширину слайдера в зависимости от кол-ва слайдов, показынных на странице
     slider.style.width = visibleSlides * slideWidth + 'px'; 
-    // console.log(visibleSlides);
 }
 
-init();
-window.addEventListener('resize', init);
-
-
+//Перемещение странице со слайдами.
 function rollSlider() {
-    // sliderLine.style.transform = "translateX(-960px)"
     sliderLine.style.transform = `translateX(-${count * slider.offsetWidth}px)`
-    // console.log(`translateX = -${count * slider.offsetWidth}px`);
 }
 
+//Клонирование элементов в конец коллекции
+function addFirstToEnd() {
+    for(let i = 0; i < slides.length; i++) {
+        let cloneItem = slidesList[i].cloneNode(true);
+        slidesList[i].parentNode.appendChild(cloneItem);
+    }
+}
 
+// Функция, которая чистит от клонов коллекцию
+function removeClones() {
+    for (let i = slidesList.length - 1; i >= slides.length; i--) {
+        slidesList[i].remove();
+    }
+}
 
 nextBtn.addEventListener('click', () => {
     count++;
+    // if(count >= slides.length / visibleSlides) {
+    //     count = 0;
+    // }
     if(count >= slides.length / visibleSlides) {
-        count = 0;
+        addFirstToEnd();
     }
     rollSlider();
+    console.log(count);
 })
 
 prevBtn.addEventListener('click', () => {
@@ -59,4 +70,11 @@ prevBtn.addEventListener('click', () => {
         count = slides.length / visibleSlides - 1;
     }
     rollSlider();
+
+    if (count <= 1) {
+        setTimeout(removeClones, 2000) 
+    }
 })
+
+init();
+window.addEventListener('resize', init);
